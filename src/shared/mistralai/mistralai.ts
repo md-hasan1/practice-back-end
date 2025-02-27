2ace79718663e26df4d05a44234e47914f4085ed
md-hasan-akon1 <akonhasan680@gmail.com>
2025-02-27 17:21:07 +0600

    update

---
+++ src/shared/mistralai/mistralai.ts
@@ -0,0 +1,31 @@
+import { Mistral } from '@mistralai/mistralai';
+// Xg47haGtcYpHLGOqG9Iysp23Esl2tvp6
+const apiKey = "Xg47haGtcYpHLGOqG9Iysp23Esl2tvp6";
+export const mistralai =async()=>{
+  const client = new Mistral({apiKey: apiKey});
+
+  const chatResponse:any = await client.chat.complete({
+    model: 'mistral-large-latest',
+    messages: [{role: 'user', content: 'tell me 5 august 2024 of bangladesh'}],
+  });
+  
+  console.log('Chat:', chatResponse.choices[0].message.content);
+}
+
+
+export const mistralai2 =async()=>{
+  const client = new Mistral({apiKey: apiKey});
+
+  const chatResponse:any = await client.chat.complete({
+    model: "pixtral-12b",
+    messages: [{role: 'user',     content: [
+        { type: "text", text: "What's in this image?" },
+        {
+          type: "image_url",
+          imageUrl: "https://tripfixers.com/wp-content/uploads/2019/11/eiffel-tower-with-snow.jpeg",
+        },
+      ],}],
+  });
+  
+  console.log('Chat:', chatResponse.choices[0].message.content);
+}
